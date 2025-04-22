cdfd8397a6d3f1315305ca2df629c79ce2df1f83
HenriqueBent0 <henrique.bt33@gmail.com>
2025-04-22 08:57:01 -0300

    fix: arrumando o "DevolucaoDAO" para as necessidades do quality gate

--- src/main/java/dao/DevolucaoDAO.java
+++ src/main/java/dao/DevolucaoDAO.java
@@ -23,24 +23,26 @@ public class DevolucaoDAO {
      */
     public ArrayList<Devolucao> getListaDevolucao() {
         listaDevolucao.clear();
-        try {
-            Connection conn = ConexaoDAO.getConexao();
-            Statement stmt = conn.createStatement();
-            ResultSet res = stmt.executeQuery("SELECT * FROM tb_devolucao");
+        String sql = "SELECT * FROM tb_devolucao";
+
+        try (Connection conn = ConexaoDAO.getConexao();
+             Statement stmt = conn.createStatement();
+             ResultSet res = stmt.executeQuery(sql)) {
+
             while (res.next()) {
                 int id = res.getInt("id");
                 String nomeAmigo = res.getString("nomeAmigo");
                 String data = res.getString("data");
                 String nomeDaFerramenta = res.getString("nomeDaFerramenta");
                 int idFerramenta = res.getInt("idFerramenta");
-
                 Devolucao devolucao = new Devolucao(nomeAmigo, idFerramenta, data, id, nomeDaFerramenta);
                 listaDevolucao.add(devolucao);
             }
-            stmt.close();
+
         } catch (SQLException ex) {
             System.out.println("Erro: " + ex);
         }
+
         return listaDevolucao;
     }
 
@@ -48,36 +50,31 @@ public class DevolucaoDAO {
      * Método para inserir uma devolução no banco de dados.
      */
     public boolean insertDevolucaoBD(Devolucao objeto) {
-        String sql = "INSERT INTO tb_devolucao(nomeAmigo, idFerramenta, nomeDaFerramenta, data, id) VALUES (?, ?, ?, ?, ?)";
-        try {
-            Connection conn = ConexaoDAO.getConexao();
+        String sqlSelectMaxId = "SELECT MAX(id) AS max_id FROM tb_devolucao";
+        String sqlInsert = "INSERT INTO tb_devolucao(nomeAmigo, idFerramenta, nomeDaFerramenta, data, id) VALUES (?, ?, ?, ?, ?)";
 
-            // Descobre o próximo ID
-            String sqlUltimoId = "SELECT MAX(id) AS max_id FROM tb_devolucao";
-            Statement stmtUltimoId = conn.createStatement();
-            ResultSet rsUltimoId = stmtUltimoId.executeQuery(sqlUltimoId);
+        try (Connection conn = ConexaoDAO.getConexao();
+             Statement stmtUltimoId = conn.createStatement();
+             ResultSet rsUltimoId = stmtUltimoId.executeQuery(sqlSelectMaxId)) {
 
             int ultimoId = 0;
             if (rsUltimoId.next()) {
                 ultimoId = rsUltimoId.getInt("max_id");
             }
-            rsUltimoId.close();
-            stmtUltimoId.close();
 
             int novoId = ultimoId + 1;
 
-            // Insere a devolução
-            PreparedStatement stmt = conn.prepareStatement(sql);
-            stmt.setString(1, objeto.getNomeAmigo());
-            stmt.setInt(2, objeto.getIdFerramenta());
-            stmt.setString(3, objeto.getNomeDaFerramenta());
-            stmt.setString(4, objeto.getData());
-            stmt.setInt(5, novoId);
-
-            stmt.execute();
-            stmt.close();
+            try (PreparedStatement stmtInsert = conn.prepareStatement(sqlInsert)) {
+                stmtInsert.setString(1, objeto.getNomeAmigo());
+                stmtInsert.setInt(2, objeto.getIdFerramenta());
+                stmtInsert.setString(3, objeto.getNomeDaFerramenta());
+                stmtInsert.setString(4, objeto.getData());
+                stmtInsert.setInt(5, novoId);
+                stmtInsert.execute();
+            }
 
             return true;
+
         } catch (SQLException erro) {
             System.out.println("Erro: " + erro);
             throw new RuntimeException(erro);
