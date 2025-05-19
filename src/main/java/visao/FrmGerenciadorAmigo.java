53096c1957b7a505e0d69c4ea377a373cf75f6a4
HenriqueBent0 <henrique.bt33@gmail.com>
2025-05-19 19:44:34 -0300

    fix: quality gate new code coverage "FrmGerenciadorAmigo"

--- src/main/java/visao/FrmGerenciadorAmigo.java
+++ src/main/java/visao/FrmGerenciadorAmigo.java
@@ -1,12 +1,8 @@
 package visao;
 
 import controle.GerenciadorAmigoController;
-import javax.swing.JButton;
-import javax.swing.JLabel;
 import javax.swing.JOptionPane;
-import javax.swing.JScrollPane;
 import javax.swing.JTable;
-import javax.swing.JTextField;
 import servico.AmigoService;
 
 /**
@@ -290,15 +286,12 @@ public class FrmGerenciadorAmigo extends javax.swing.JFrame {
     public void carregaTabela() {
         controller.carregarTabela();
     }
-
     public void carregarTabelaGerenciador() {
         controller.carregarTabelaGerenciador();
     }
-
     public void mostrarMensagem(String mensagem) {
         JOptionPane.showMessageDialog(null, mensagem);
     }
-
     public String getMensagem() {
         return mensagem;
     }
@@ -307,88 +300,9 @@ public class FrmGerenciadorAmigo extends javax.swing.JFrame {
         this.mensagem = mensagem;
     }
 
-    public void setController(GerenciadorAmigoController controller) {
-        this.controller = controller;
-    }
-
-    public void setJBApagar(JButton JBApagar) {
-        this.JBApagar = JBApagar;
-    }
-
-    public void setJBCancelar(JButton JBCancelar) {
-        this.JBCancelar = JBCancelar;
-    }
-
-    public void setJBEditar(JButton JBEditar) {
-        this.JBEditar = JBEditar;
-    }
-
-    public void setJTFNome(JTextField JTFNome) {
-        this.JTFNome = JTFNome;
-    }
-
-    public void setJTFTelefone(JTextField JTFTelefone) {
-        this.JTFTelefone = JTFTelefone;
-    }
 
     public void setJTableAmigos(JTable JTableAmigos) {
         this.JTableAmigos = JTableAmigos;
-    }
-
-    public void setjLabel1(JLabel jLabel1) {
-        this.jLabel1 = jLabel1;
-    }
-
-    public void setjLabel2(JLabel jLabel2) {
-        this.jLabel2 = jLabel2;
-    }
-
-    public void setjLabel3(JLabel jLabel3) {
-        this.jLabel3 = jLabel3;
-    }
-
-    public void setjLabel4(JLabel jLabel4) {
-        this.jLabel4 = jLabel4;
-    }
-
-    public void setjScrollPane1(JScrollPane jScrollPane1) {
-        this.jScrollPane1 = jScrollPane1;
-    }
-
-    public GerenciadorAmigoController getController() {
-        return controller;
-    }
-
-    public JButton getJBApagar() {
-        return JBApagar;
-    }
-
-    public JButton getJBCancelar() {
-        return JBCancelar;
-    }
-
-    public JButton getJBEditar() {
-        return JBEditar;
-    }
-
-    public JLabel getjLabel1() {
-        return jLabel1;
-    }
-
-    public JLabel getjLabel2() {
-        return jLabel2;
-    }
-
-    public JLabel getjLabel3() {
-        return jLabel3;
-    }
-
-    public JLabel getjLabel4() {
-        return jLabel4;
-    }
-
-    public JScrollPane getjScrollPane1() {
-        return jScrollPane1;
     }
 
 }
